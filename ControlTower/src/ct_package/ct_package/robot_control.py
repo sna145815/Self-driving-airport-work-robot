186e031268eca505fdb6b3a4832ed193c4d99a38
cccsssshh <shcho9666@gmail.com>
2024-05-23 20:00:16 +0900

    add launch file, and node refactoring

--- ControlTower/src/ct_package/ct_package/robot_control.py
+++ ControlTower/src/ct_package/ct_package/robot_control.py
@@ -1,15 +1,18 @@
 import rclpy as rp
 from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+
 from interface_package.msg import Navfeedback, Navgoal, Navresult
 from interface_package.srv import Tcp
 from interface_package.msg import OrderLocation
 from std_msgs.msg import String
+from interface_package.srv import MotorControl,LCDControl,RFIDControl 
 
 
-class RobotControl(Node):
+class Motor(Node):
 
     def __init__(self):
-        super().__init__("robot_control")
+        super().__init__("motor")
 
         self.order_location_sub = self.create_subscription(OrderLocation, "robot_goal", self.order_location_callback, 10)
         self.arrival_pub = self.create_publisher(String, 'goal_arrival', 10)
@@ -62,41 +65,143 @@ class RobotControl(Node):
         self.goal_pub.publish(self.goal_msg)
 
 
+class RFID(Node):
 
-class UidNode(Node):
     def __init__(self):
-        super().__init__('uid_node')
+        super().__init__('rfid')
+        self._client = self.create_client(RFIDControl, 'rfid_control')
+        self.sub = self.create_subscription(String,'/uid',self.callback,10)
+        self.sub
+        self.uid=''
+        self._motor_client = Servo()
 
-        self.srv = self.create_service(Tcp, 'tcp_string', self.handle_request)
-        self.pub = self.create_publisher(String, '/uid', 10)
+        while not self._client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('RFID service not available, waiting again...')
+    
+    def callback(self,msg):
+        self.uid = msg.data
+        print("receive uid from Kiosk")
+
+    def send_request(self):
+        request = RFIDControl.Request()
+        future = self._client.call_async(request)
+        future.add_done_callback(self.handle_response)
+
+    def handle_response(self, future):
+        try:
+            response = future.result()
+
+            if response.success:
+                self.rid = hex(response.id)
+                self.rid = self.hex_string_to_byte_array(self.rid[2:-2])
+                self.get_logger().info(f'Successfully received RFID ID: {self.rid}')
+                if self.rid == self.uid:
+                    self._motor_client.run()
+
+                self.send_request()
+            else:
+                self.get_logger().warning('Failed to receive RFID ID')
+
+                self.send_request()
+        except Exception as e:
+            self.get_logger().warning(f'Service call failed: {e}')
+            self.send_request()
+
+    def hex_string_to_byte_array(self,hex_string):
+        byte_array = [hex_string[i:i+2].upper() for i in range(0, len(hex_string), 2)]
+        byte_array_str = ' '.join(byte_array)
+        return byte_array_str
 
 
-    def uid_publish(self, uid):
-        msg = String()
-        msg.data = uid
-        self.pub.publish(msg)
-        self.get_logger().info(f"published message : {uid}")
+class Servo(Node):
 
-    def handle_request(self, request, response):
-        # 요청을 로그에 출력
-        self.get_logger().info(f'Received request: {request.uid}')
-        response.response = 'Request received successfully.'
+    def __init__(self):
+        super().__init__('servo')
+        self._client = self.create_client(MotorControl, 'motor_control')
+        self.state="close"
 
-        uid = request.uid
+        while not self._client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
 
-        self.uid_publish(uid)
+    def send_request(self, command):
+        self.get_logger().info("Sending motor command...")
+        request = MotorControl.Request()
+        request.command = command
 
-        return response
+        self.get_logger().info(f'Sending command: {command}')
+        future = self._client.call_async(request)
+        future.add_done_callback(self.handle_response)
 
+    def handle_response(self, future):
+        try:
+            self.get_logger().info(f'Success: {future.result().success}')
+        except Exception as e:
+            self.get_logger().warning('Service call failed! %r' % e)
+
+    def run(self):
+        if self.state=='close':
+            self.send_request('open')
+            self.state='open'
+        else:
+            self.send_request('close')
+            self.state='close'
+
+
+class LCD(Node):
+
+    def __init__(self):
+        super().__init__('lcd')
+        self._client = self.create_client(LCDControl, 'lcd_control')
+
+        while not self._client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+
+    def send_request(self, line1, line2):
+        self.get_logger().info("Sending LCD command...")
+        request = LCDControl.Request()
+        request.line1 = line1
+        request.line2 = line2
+
+        self.get_logger().info(f'Sending lines: {line1}, {line2}')
+        future = self._client.call_async(request)
+        future.add_done_callback(self.handle_response)
+
+    def handle_response(self, future):
+        try:
+            self.get_logger().info(f'Success: {future.result().success}')
+        except Exception as e:
+            self.get_logger().warning('Service call failed! %r' % e)
+
+    def run(self):#input
+        while True:
+            line1 = input("Enter text for line 1: ")
+            line2 = input("Enter text for line 2: ")
+            self.send_request(line1, line2)
 
 
 def main(args=None):
     rp.init(args=args)
-    node = RobotControl()
+    motor_node = Motor()
+    rfid_node = RFID()
+    servo_node = Servo()
+    lcd_node = LCD()
 
-    rp.spin(node)
-    node.destroy_node()
-    rp.shutdown()
+    executor = MultiThreadedExecutor()
+
+    executor.add_node(motor_node)
+    executor.add_node(rfid_node)
+    executor.add_node(servo_node)
+    executor.add_node(lcd_node)
+
+    try:
+        executor.spin()
+    finally:
+        executor.shutdown()
+        motor_node.destroy_node()
+        rfid_node.destroy_node()
+        servo_node.destroy_node()
+        lcd_node.destroy_node()
+        rp.shutdown()
 
 if __name__ == "__main__":
     main()
--- DRobot/pinkbot/src/drobot_package/drobot_package/lcd_controller.py
+++
@@ -1,8 +0,0 @@
-import RPi_I2C_driver
-
-class LCDController:
-    def __init__(self):
-        self.mylcd = RPi_I2C_driver.lcd()
-
-    def display_string(self, string, line):
-        self.mylcd.lcd_display_string(string, line)
