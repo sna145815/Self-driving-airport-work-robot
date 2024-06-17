e8002f41f76a0ddf28bdeebb0f25d01ee5b4a8c8
earnest <12yhkim@naver.com>
2024-06-17 14:50:45 +0900

    add robot pacakage

--- DRobot/pinkbot/src/drobot_package/drobot_package/drobot_module.py
+++ DRobot/pinkbot/src/drobot_package/drobot_package/drobot_module.py
@@ -4,22 +4,22 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import rclpy
 from rclpy.node import Node
-from interface_package.srv import OrderTracking,OrderInfo
+from interface_package.srv import OrderTracking,OrderInfo,Module
 from servo_controller import ServoController
 from lcd_controller import LCDController  
 from time import sleep
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from std_msgs.msg import Int16
 
-ROBOT_NUM = 1
+
+ROBOT_NUM = 3
 
 class DRobotModule(Node):
     def __init__(self):
         super().__init__('drobot_module')
         self.order_service = self.create_service(OrderInfo, 'order_info', self.order_callback)
         self.order_tracking_client = self.create_client(OrderTracking,'order_tracking')
-        self.robot_flag = self.create_subscription(Int16,'/status',self.callback,10)
+        self.robot_flag = self.create_service(Module,'module',self.callback)
         self.servo_controller = ServoController()
         self.servo_controller.move_to_zero()
         self.lcd_controller = LCDController()
@@ -38,13 +38,15 @@ class DRobotModule(Node):
         response.success = True
         self.get_logger().info(f'received! UID : {self.uid}  order_ID : {self.order_id}')
         self.lcd_controller.display_string(f"Order No. {self.order_id} ", 2)
+        self.send_request("DY") #
         print(f"order_id : {self.order_id}")
         return response
     
-    def callback(self, msg):
-        if (self.robot_status != msg.data and (msg.data == 2 or msg.data == 4)):
-            print(f"{self.robot_status}------{msg.data}")
-            
+    def callback(self, request, response):
+        if request.data == 'HM': #
+            self.send_request("DY") #
+            response.success = True #
+        else : # 
             self.get_logger().info("scan RFID")
             
             self.lcd_controller.display_string_at_position("Tag me", 9, 1)
@@ -63,28 +65,34 @@ class DRobotModule(Node):
                             
                         else:
                             self.servo_controller.move_to_zero()
-                            if msg.data == 2:
+                            if request.data == 'ST':
                                 self.send_request("DS") 
                                 self.get_logger().info("Delivery Start")
                             
-                            elif msg.data == 4:
+                            elif request.data == 'KS':
                                 self.send_request("DF") 
                                 self.get_logger().info("Delivery Finish")
+                                self.lcd_controller.display_string("           ", 2)#
 
+                                
                             self.lcd_controller.display_string_at_position(" "*6, 9, 1)
                             self.count = 0 
-                            self.robot_status = msg.data
                             break
                         sleep(1)
                         
 
                     else:
                         self.get_logger().info("permission denied!")
+                        self.lcd_controller.display_string_at_position("wrong ID", 8, 1)
+                        sleep(2)
+                        self.lcd_controller.display_string_at_position(" "*7, 8, 1)
                     
+                response.success = True
             except Exception as e:
                 self.get_logger().error(f'Error reading RFID: {str(e)}')
+                response.success = False
+        return response #
 
-        self.robot_status = msg.data
 
     def hex_string_to_byte_array(self,hex_string):
         byte_array = [hex_string[i:i+2].upper() for i in range(0, len(hex_string), 2)]
